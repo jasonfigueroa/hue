88017c95f75c3336152ca64e825b11983bd3a23f
Jason Figueroa <jason.figueroa2@gmail.com>
2018-03-22 06:08:30 -0500

    initial commit

---
+++ hue/LightInterface.cs
@@ -0,0 +1,74 @@
+﻿using Q42.HueApi;
+using Q42.HueApi.ColorConverters;
+using Q42.HueApi.ColorConverters.HSB;
+using Q42.HueApi.Interfaces;
+using Q42.HueApi.Models.Groups;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace hue
+{
+    class LightInterface
+    {
+        public static bool alive;
+
+        public static async Task BombActive()
+        {
+            ILocalHueClient client = new LocalHueClient("192.168.1.104");
+            client.Initialize("7R70D9970VOrmdffj3qwNgUBpNwu4d7I12IZKTqj");
+            var command = new LightCommand();
+            command.TurnOn().SetColor(new RGBColor("FF0000"));
+            command.Alert = Alert.Multiple;
+
+            while (alive)
+            {
+                await client.SendCommandAsync(command);
+                Thread.Sleep(7000);
+            }
+            command.TurnOff();
+            command.Alert = Alert.None;
+            await client.SendCommandAsync(command);
+        }
+
+        public static async Task BombDefused()
+        {
+            ILocalHueClient client = new LocalHueClient("192.168.1.104");
+            client.Initialize("7R70D9970VOrmdffj3qwNgUBpNwu4d7I12IZKTqj");
+            var command = new LightCommand();
+            command.TurnOn().SetColor(new RGBColor("0000FF"));
+
+            command.Alert = Alert.None;
+
+            await client.SendCommandAsync(command);
+
+            Thread.Sleep(10000);
+        }
+
+        public static async Task BombExplode()
+        {
+            ILocalHueClient client = new LocalHueClient("192.168.1.104");
+            client.Initialize("7R70D9970VOrmdffj3qwNgUBpNwu4d7I12IZKTqj");
+            var command = new LightCommand();
+            command.TurnOn().SetColor(new RGBColor("FFA500"));
+
+            command.Alert = Alert.None;
+
+            await client.SendCommandAsync(command);
+
+            Thread.Sleep(10000);
+        }
+
+        public static async Task TurnLightOff()
+        {
+            ILocalHueClient client = new LocalHueClient("192.168.1.104");
+            client.Initialize("7R70D9970VOrmdffj3qwNgUBpNwu4d7I12IZKTqj");
+            var command = new LightCommand();
+            command.TurnOff();
+
+            await client.SendCommandAsync(command);
+        }
+    }
+}
---
+++ hue/Program.cs
@@ -0,0 +1,28 @@
+﻿using Q42.HueApi;
+using Q42.HueApi.Interfaces;
+//using Q42.HueApi.ColorConverters.Original;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Q42.HueApi.ColorConverters;
+using Q42.HueApi.ColorConverters.HSB;
+using System.Threading;
+
+namespace hue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            LightInterface.alive = true;
+            LightInterface.BombActive();
+            Thread.Sleep(30000);
+            LightInterface.alive = false;
+            Console.WriteLine("bomb active complete");
+            LightInterface.BombDefused().Wait();
+            LightInterface.TurnLightOff().Wait();
+            Console.WriteLine("sequence complete");
+            Console.ReadKey();
+        }
+    }
+}
